820b73ab1158120dc985363e544b22cbf1b324f8
Gabrielly <gabrielly.amorin@ccc.ufcg.edu.br>
2020-10-14 11:16:32 -0300

    refinando a classe project *falta um metodo*

--- python/project.py
+++ python/project.py
@@ -1,83 +1,35 @@
 #coding: utf-8
 class Project:
-	def __init__(self, name, description, base_text, project_id, mentor, aux_mentor, areas):
+	def __init__(self, name, description, base_text, mentor, aux_mentor, areas):
 		self.name = name
 		self.description = description
 		self.base_text = base_text
 		self.project_id = project_id
-		self.mentor = mentor
-		self.aux_mentor = aux_mentor
+		self.mentor = "Não confirmado"
+		self.aux_mentor = "Não confirmado"
 		self.areas = areas
 		self.state = "Em análise"
 		self.subscribers = {}
 
 	@property
-	def name(self):
-		return self.name
-	
-	@name.setter
-	def name(self, name):
-		self.name = name
-
-	@property
-	def description(self):
-		return self.description
-		
-	@description.setter
-	def description(self, description):
-		self.description = description
-	
-	@property
-	def base_text(self):
-		return self.base_text
-		
-	@description.setter
-	def base_text(self, base_text):
-		self.base_text = base_text
-	
-	@property
-	def project_id(self):
-		return self.project_id
-		
-	@description.setter
-	def project_id(self, project_id):
-		self.project_id = project_id
-	
-	@property
-	def mentor(self):
-		return self.mentor
-		
-	@description.setter
-	def mentor(self, mentor):
-		self.mentor = mentor
-	
-	@property
-	def aux_mentor(self):
-		return self.aux_mentor
-		
-	@description.setter
-	def aux_mentor(self, aux_mentor):
-		self.aux_mentor = aux_mentor
-	
-	@property
 	def areas(self):
 		return self.areas
-		
-	@description.setter
-	def areas(self, areas):
-		self.areas = areas
 	
+	@areas.setter
+	def areas(self, areas):
+		if not (areas in ['front', 'back', 'cloud', 'testes', 'documentação', 'refactoring', 'design', 'análise de dados', 'scrapping', 'automação', 'bot', 'devops']):
+			raise Exception('Área Inválida')
+		self.areas = areas
+
 	@property
 	def state(self):
 		return self.state
 		
-	@description.setter
-	def state(self, state):
-		self.state = state
-	
-	@property
-	def subscribers(self):
-		return self.subscribers
+	@state.setter
+    def state(self, state):
+        if not (state in ['Em análise', 'Necessita Revisão', 'Pronto - Com sugestões', 'Pronto - Completo']):
+            raise Exception('Estado Inválido')
+        self._state = state
 		
 	def add_subscribers(self, key, value):
 		self.subscribers[key] = value
