82fa08e0bf678ef3b3641dc9efd7d13d72c8c3e4
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2020-10-14 13:52:54 -0300

    :hammer: Refatoração de alguns arquivos

--- python/subscribed.py
+++ python/subscribed.py
@@ -1,16 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from user import User
 
 
 class Subscribed(User):
     def __init__(self, name, email, discord_id, period='', minority_group=''):
         User.__init__(self, name, email, discord_id)
-        self.__period = period
-        self.__minority_group = minority_group
+        self.period = period
+        self.minority_group = minority_group
 
-    @property
-    def period(self):
-        return self.__period
-
-    @period.setter
-    def period(self, value):
-        return self.__period
--- python/user.py
+++ python/user.py
@@ -1,42 +1,31 @@
+# -*- coding: utf-8 -*-
+
 import re
-from error import *
 
 
 class User:
     def __init__(self, name='', email='', discord_id=''):
         self.name = name
-        self.__email = email
-        self.__discord_id = discord_id
+        self.email = email
+        self.discord_id = discord_id
 
     @property
     def name(self):
-        return self.__name
+        return self._name
 
     @name.setter
     def name(self, value):
-        value = value.upper()
-        self.__name = value
-
-    @property
-    def email(self):
-        return self.__email
-
-    @email.setter
-    def email(self, value):
-        self.__email = value
-
-    @property
-    def discord_id(self):
-        return self.__discord_id
-
-    @discord_id.setter
-    def discod_id(self, value):
-        return self.__discord_id
+        if isinstance(value, str):
+            value = value.upper()
+            self._name = value
+        else:
+            raise Exception('Invalid name!')
 
     def email_validacion(self):
         regex = '^[a-z]+[\._]?[a-z]+[@]{1}[c]{3}[.][c-u]{4}[.][d-u]{3}[.][b-r]{2}$'
-        try:
-            if re.search(regex, self.__email):
-                email_error()
-        except EmailError as error:
-            print(error)
+
+        if re.search(regex, self.email):
+            return self.email
+        else:
+            raise Exception('Invalid mail!')
+            
