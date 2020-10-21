c488443b8334ef0fd6da3592c33ff51d6d97fced
Huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2020-10-21 18:59:51 -0300

    Corrigindo caminhos das importaçoes

--- python/mentor.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from user import User
-
-
-class Mentor(User):
-    def __init__(self, name, email, discord_id, state='',organization=''):
-        User.__init__(self, name, email, discord_id)
-        self.state = state
