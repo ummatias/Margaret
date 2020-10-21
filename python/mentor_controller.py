b2dc94c9639b6178fbabd413cb11977e7232d419
Huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2020-10-21 19:02:02 -0300

    Ajeitando formataçao de alguns arquivos

--- python/mentor_controller.py
+++ python/mentor_controller.py
@@ -1,50 +1,48 @@
 # -*- coding: utf-8 -*-
-from mentor import Mentor
+from models.mentor import Mentor
+
 
 class mentor_controller:
-
     def __init__(self):
         self.mentors = {}
 
-    def add_mentor(self,name,email,discord,state):
+    def add_mentor(self, name, email, discord, state):
         self.validating_existing_email(email)
-        mentor = Mentor(name,email,discord,state)
+        mentor = Mentor(name, email, discord, state)
         self.mentors[email] = mentor
         return self.mentors[email]
-    
+
     def list_mentor(self):
         return self.mentors.values()
 
-    def find_mentors_by_attribute(self,attribute,value_attribute):
-        if attribute.lower() not in['name','email','discord','state','organization']:
+    def find_mentors_by_attribute(self, attribute, value_attribute):
+        if attribute.lower() not in ['name', 'email', 'discord', 'state', 'organization']:
             raise AttributeError("Atributo inválido")
-        
+
         found_mentors = []
 
         for mentor in self.mentors.values():
-            if value_attribute.lower() in getattr(mentor,attribute):
+            if value_attribute.lower() in getattr(mentor, attribute):
                 found_mentors.append(mentor)
 
         return found_mentors
 
-
-    def modify_mentor_by_attribute(self,email_user,attribute,new_attribute):
-        if attribute.lower() not in['name','email','discord','state','organization']:
+    def modify_mentor_by_attribute(self, email_user, attribute, new_attribute):
+        if attribute.lower() not in ['name', 'email', 'discord', 'state', 'organization']:
             raise AttributeError("Atributo inválido")
 
-        self.validate_mentor_existence(email_user)            
+        self.validate_mentor_existence(email_user)
 
-        setattr(self.mentors[email_user],attribute,new_attribute)
-        
+        setattr(self.mentors[email_user], attribute, new_attribute)
 
-    def remove_mentor(self,email_user):
+    def remove_mentor(self, email_user):
         self.validate_mentor_existence(email_user)
         del self.mentors[email_user]
 
-    def validate_mentor_existence(self,email_user):
+    def validate_mentor_existence(self, email_user):
         if not email_user in self.mentors.keys():
             raise ValueError("Usuário não inscrito")
 
-    def validating_existing_email(self,email):
+    def validating_existing_email(self, email):
         if email in self.mentors.keys():
             raise ValueError("Email já em uso")
