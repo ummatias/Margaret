6bca400816940802883f0e67e0289975f3a59d57
Gabrielly <gabrielly.amorin@ccc.ufcg.edu.br>
2020-10-21 17:06:35 -0300

    finalizado a classe project, teste da classe project e primeira rota feitos

--- python/project.py
+++ python/project.py
@@ -1,10 +1,10 @@
 #coding: utf-8
+
 class Project:
-	def __init__(self, name, description, base_text, areas):
+	def __init__(self, name, description, mentor, aux_mentor, base_text, areas):
 		self.name = name
 		self.description = description
 		self.base_text = base_text
-		self.project_id = project_id
 		self.mentor = mentor
 		self.aux_mentor = aux_mentor
 		self.areas = areas
@@ -13,26 +13,36 @@ class Project:
 
 	@property
 	def areas(self):
-		return self.__areas
+		return self._areas
 	
 	@areas.setter
 	def areas(self, areas):
 		if not (areas in ['front', 'back', 'cloud', 'testes', 'documentação', 'refactoring', 'design', 'análise de dados', 'scrapping', 'automação', 'bot', 'devops']):
 			raise Exception('Área Inválida')
-		self.__areas = areas
+		self._areas = areas
 
 	@property
 	def state(self):
-		return self.__state
+		return self._state
 		
 	@state.setter
-    def state(self, state):
-        if not (state in ['Em análise', 'Necessita Revisão', 'Pronto - Com sugestões', 'Pronto - Completo']):
-            raise Exception('Estado Inválido')
-        self.__state = state
+	def state(self, state):
+		if not (state in ['Em análise', 'Necessita Revisão', 'Pronto - Com sugestões', 'Pronto - Completo']):
+			raise Exception('Estado Inválido')
+		self._state = state
 		
-	def add_subscribers(self, key, value):
-		self.subscribers[key] = value
+	def add_subscriber(self, key, value):
+		if key not in self.subscribers:
+			self.subscribers[key] = value
+		else:
+			key += 1
+			self.subscribers[key] = value
 	
-	def get_subscribers(self, key):
+	def get_subscriber(self, key):
 		return self.subscribers[key]
+	
+	def check_email(self, email):
+		mentor = self.mentor
+		if email == mentor["email"]:
+			return True
+		return False
