740dc8a96362fd4909f47970d3567560ba789c61
Caio Davi <caio.silva@ccc.ufcg.edu.br>
2020-10-26 16:03:03 -0300

    tests_user, validation and user

--- python/models/user.py
+++ python/models/user.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-
-from util import validation
-
-
+from python.util import validation
 
 class User:
     def __init__(self, name='', email='', discord_id=''):
@@ -17,13 +14,28 @@ class User:
     @name.setter
     def name(self, value):
         if isinstance(value, str):
+            validation.empty_validation(value,'Nome')
             value = value.upper()
             self._name = value
         else:
-            raise Exception('Invalid name!')
+            raise AttributeError('Nome inválido')
+
+    @property
+    def email(self):
+        return self._email
 
     @email.setter
     def email(self,value):
         validation.email_validation(value)
-        self.email = value
+        self._email = value
 
+    @property
+    def discord_id(self):
+        return self._discord_id
+
+    @discord_id.setter
+    def discord_id(self, value):
+        if isinstance(value, str):
+            validation.empty_validation(value, 'Discord ID')
+            validation.discord_id_validation(value)
+            self._discord_id = value 
--- python/util/validation.py
+++ python/util/validation.py
@@ -2,9 +2,24 @@
 import re
 
 def email_validation(email):
-        regex = '^[a-z]+[\._]?[a-z]+[@]{1}[c]{3}[.][c-u]{4}[.][d-u]{3}[.][b-r]{2}$'
-        try:
-            if re.search(regex,email):
-                return email        
-        except:
-            raise Exception("Email inválido")
+    if isinstance(email, str):
+        regex = r'^[a-z]+[\._]?[a-z]+[@]{1}[c]{3}[.][c-u]{4}[.][d-u]{3}[.][b-r]{2}$'
+
+        if re.search(regex, email) != None:
+            return email
+
+    raise AttributeError('Email inválido!')
+
+def empty_validation(value, atributo):
+    if value == "":
+        raise AttributeError(atributo + " não pode ser vazio!") 
+
+def discord_id_validation(value):
+    if "#" in value:
+        numbers = value.split("#")
+        regexNumbers = r'^[0-9]{4}$'
+
+        if  re.search(regexNumbers,numbers[1]) != None:
+            return value
+        
+    raise AttributeError ("Discord inválido!")
