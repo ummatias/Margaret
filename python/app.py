edfdaeb74f1f3fb0a24126912da2ff365b3eccc7
Juan Barros <juanlyrabarros@gmail.com>
2020-10-28 18:37:47 -0300

    Versao 2

--- python/app.py
+++ python/app.py
@@ -1,14 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from python.models.project import Project
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
-def index():
+def get():
     proj = Project("Margaret", "sistem andromedev",
                    "projeto opensource", 1, "juan", "renan", ["back", "front"])
     return jsonify(proj.__dict__), 200
+
+
+@app.route("/", methods=["POST"])
+def post():
+    data = request.get_json()
+
+    return {"name": data["nome"]}, 200
+
+
+@app.route("/", methods=["PUT"])
+def put():
+    data = request.get_json()
+
+    return {"name": data["nome"]+"a"}
 
 
 if __name__ == "__main__":
