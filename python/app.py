4631fc987f2ac78c27a1c848c47be53416387578
ummatias <mateus.ribeiro@ccc.ufcg.edu.br>
2020-10-28 19:21:01 -0300

    Add ambiente virtual e rota teste  subscribed

--- python/app.py
+++ python/app.py
@@ -1,13 +1,23 @@
-from flask import Flask, jsonify
-from python.project import Project
+from flask import Flask
+from python.models.subscribed import Subscribed
+from python.models.project import Project
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=["GET"])
+@app.route('/', methods=['GET'])
 def index():
-    proj = Project("Margaret", "sistem andromedev", "projeto opensource", "back")
-    return jsonify(proj.__dict__)
+    return 'rotas:   /subscribed   -   /project'
+
+@app.route('/subscribed', methods=['GET'])
+def subscriber():
+    subscribed = Subscribed('All Might', 'arumaito.smash@ccc.ufcg.edu.br', 'Arumaito#4586', '2002.1', '')
+    return subscribed.__dict__
+
+
+@app.route("/project", methods=["GET"])
+def project():
+    proj = Project('Margaret', 'sistem andromedev', 'projeto opensource', 'back')
+    return proj.__dict__
 
 
 if __name__ == "__main__":
